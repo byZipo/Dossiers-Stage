603f1fd83be3939774f49d5648d865bea566ee6c
Thibault <th.delavelle@gmail.com>
2017-03-31 14:50:29 +0200

    j'avais oublié de add la MainClass

---
+++ Travail_realise/Programmes/ImageJ/plugins/MainClass.java
@@ -0,0 +1,34 @@
+package RegionGrow.main;
+
+import java.nio.file.Path;
+
+import javax.swing.JFileChooser;
+
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+
+
+//classe pour executer ImageJ et le plugin de segmentation sans passer par l'interface ImageJ
+public class MainClass {
+
+	public MainClass(){
+		//selection de l'image a segmenter
+		//le repertoire par defaut du JFileChooser est constitue a l'aide de \\ et non pas des \
+		JFileChooser dialogue = new JFileChooser("C:\\Users\\Thibault\\Documents\\M2-Info\\Stage\\Images\\CT");
+		Path path = null;
+		if (dialogue.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
+			 path = dialogue.getSelectedFile().toPath();
+		}
+		ImagePlus im = new ImagePlus(path.toString());
+		Croissance_Regions c = new Croissance_Regions();
+		//il suffit de faire appel aux methodes de base d'un plugin ImageJ : setup() et run()
+		c.setup("", im);
+		ImageProcessor i = im.getProcessor();
+		c.run(i);
+	}
+	
+	public static void main(String[] args) {
+		new MainClass();
+	}
+	
+}
